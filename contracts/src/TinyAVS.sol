88c83fe211a32e4bd8ea50d096e37bb49145a186
Wes Floyd <wesfloyd@gmail.com>
2025-04-01 13:39:50 -0400

    updates

--- contracts/src/TinyAVS.sol
+++ contracts/src/TinyAVS.sol
@@ -1,21 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "../lib/eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
+contract TinyAVS is ServiceManagerBase {
+    constructor(
+        IAVSDirectory _avsDirectory,
+        IRewardsCoordinator _rewardsCoordinator,
+        ISlashingRegistryCoordinator _slashingRegistryCoordinator,
+        IStakeRegistry _stakeRegistry,
+        IPermissionController _permissionController,
+        IAllocationManager _allocationManager
+    )
+        ServiceManagerBase(
+            _avsDirectory,
+            _rewardsCoordinator,
+            _slashingRegistryCoordinator,
+            _stakeRegistry,
+            _permissionController,
+            _allocationManager
+        )
+    {}
 
-contract TinyAVS {
-    IAllocationManager public immutable allocationManager;
+    function initialize(
+        address initialOwner,
+        address rewardsInitiator
+    ) public virtual initializer {
+        __ServiceManagerBase_init(initialOwner, rewardsInitiator);
+    }
 
-    constructor(address _allocationManager) {
-        allocationManager = IAllocationManager(_allocationManager);
+    function slashOperator(
+        IAllocationManager.SlashingParams memory _params
+    ) public {
+        _allocationManager.slashOperator({avs: address(this), params: _params});
     }
 
     function getAmIAnAVS() public pure returns (string memory) {
         return "yes, I am a tiny AVS";
     }
 
-    function updateMetadataURI(string calldata newMetadataURI) external {
-        // Call the AllocationManager to update the metadata URI for this AVS
-        allocationManager.updateAVSMetadataURI(address(this), newMetadataURI);
-    }
 }
